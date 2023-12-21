c93e88bcddf7152022b1b1bf7b39967ce87c3406
JuHeonHa <hjhjaueon97@gmail.com>
2023-12-21 23:51:26 +0900

    feat : Trend component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,45 @@
 <template>
-  <div>Home</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
+    </div>
+  </section>
+  <section
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
+  >
+    <Trend
+      color="green"
+      title="Income"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Income"
+      :amount="400"
+      :last-amount="100"
+      :loading="false"
+    />
+    <Trend
+      color="green"
+      title="Income"
+      :amount="400"
+      :last-amount="333"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Income"
+      :amount="400"
+      :last-amount="122"
+      :loading="false"
+    />
+  </section>
 </template>
+<script setup>
+import { transactionViewOptions } from "~/constants";
+
+const selectedView = ref(transactionViewOptions[1]);
+</script>
